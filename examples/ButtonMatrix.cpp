9d0c2f724de9c32cc5c5bc57c2080700c2287a52
Franklyn Vasquez <franklynvasquez24@gmail.com>
2023-09-17 13:01:09 -0400

    wrote example, metadata

---
+++ examples/ButtonMatrix.cpp
@@ -0,0 +1,46 @@
+/*
+	PinMatrix is a wrapper around grid matrices.
+*/
+
+//Required library include. Must be BEFORE includeing PinMatrix.h
+#include <SketchBinder.h>
+
+#include <PinMatrix.h>
+
+//My row and column pins. Pin arrays must not be pointers and must be const
+const byte rowCount = 2;
+const byte columnCount = 3;
+
+const byte rowPins[] = { 2, 3 };
+const byte columnPins[] = { 4, 5, 6 };
+
+//Debouncing time
+const int debounce = 20;
+
+//Clean way:
+auto buttonMatrix = createMatrix(rowPins, columnPins, debounce);
+
+//Verbose way:
+//PinMatrix<rowCount, columnCount> buttonMatrix = PinMatrix<rowCount, columnCount>(rowPins, columnPins, debounce);
+
+void setup() {
+	Serial.begin(115200);
+	buttonMatrix.updateHandler = buttonUpdated;
+}
+
+void loop() {
+}
+
+void buttonUpdated(byte row, byte column, bool state) {
+	Serial.print("button on row ");
+	Serial.print(row);
+	Serial.print(", column ");
+	Serial.print(column);
+	Serial.print(" was ");
+
+	if (state) {
+		Serial.println("pushed.");
+	} else {
+		Serial.println("released.");
+	}
+}
